25c8b7690c938289b1e5e13bf179495522acd39c
mjmersenski <mjmersenski@192.168.1.2>
2017-09-12 21:31:40 -0500

    added properties and validation to Bicycle in task1

--- src/task1/Bicycle.java
+++ src/task1/Bicycle.java
@@ -1,5 +1,34 @@
 package task1;
 
 public class Bicycle {
+    private int frameSize;
+    private String frameMaterial;
+    
+    public double getFrameCost(){
+        return 0;   //set method in child classes
+    }
+
+    public int getFrameSize() {
+        return frameSize;
+    }
+
+    public void setFrameSize(int frameSize) {
+        if(frameSize < 10 || frameSize > 68){
+            throw new IllegalArgumentException("That is not a valid frame size");
+        }
+        this.frameSize = frameSize;
+    }
+
+    public String getFrameMaterial() {
+        return frameMaterial;
+    }
+
+    public void setFrameMaterial(String frameMaterial) {
+        if(frameMaterial != null || frameMaterial.isEmpty()){
+            throw new IllegalArgumentException("cannot be left blank");
+        }
+        this.frameMaterial = frameMaterial;
+    }
+    
     
 }
